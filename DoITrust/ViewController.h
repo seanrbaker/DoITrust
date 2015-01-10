db8d8e69bd2398fb2b0c2975cc58b133e29f88d2
Sean Baker <sean.baker@usuhs.edu>
2015-01-10 11:50:18 -0500

    Initial Commit

---
+++ DoITrust/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DoITrust
+//
+//  Created by Sean Baker on 1/10/15.
+//  Copyright (c) 2015 Sean Baker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DoITrust/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DoITrust
+//
+//  Created by Sean Baker on 1/10/15.
+//  Copyright (c) 2015 Sean Baker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
